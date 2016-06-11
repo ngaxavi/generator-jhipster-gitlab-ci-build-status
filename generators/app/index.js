993a333e124b74fd471583cea7c869b39cc0b273
Xavier Ngansop <xng@cs.uni-kassel.de>
2016-06-12 01:02:13 +0200

    add templates and prompting message

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,3 +1,23 @@
-/**
- * Created by ngansop on 11.06.16.
- */
+"use strict";
+
+var yeoman = require("yeoman-generator");
+var chalk = require("chalk");
+var yosay = require("yosay");
+
+module.exports = yeoman.Base.extend({
+    templates: function(args) {
+        this.composeWith('jhipster:modules', {
+            options: {
+                jhipsterVar: jhipsterVar,
+                jhipsterFunc: jhipsterFunc
+            }
+        });
+    },
+    prompting: function () {
+        var done = this.async();
+
+        this.log(yosay("Welcome to the " + chalk.red('JHipster CI Gitlab Status') + "generator!"))
+    }
+});
+
+
