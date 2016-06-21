fc3ba2701fb100885a7c7bbef1b4536f29c342a9
Xavier Ngansop <xng@cs.uni-kassel.de>
2016-06-21 22:46:37 +0200

    insert the build status into the navbar

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,6 +3,7 @@
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
+var cheerio = require('cheerio');
 var packageJson = require(__dirname + '/../../package.json');
 
 // stores JHipster variables
@@ -30,23 +31,54 @@ module.exports = yeoman.Base.extend({
 
     //TODO: Check if JHipster application contains the gitlab-ci file
 
-    var prompts = [{
+    var prompts = [
+      {
       type: 'confirm',
       name: 'confirm',
       message: 'Would you like to enable Gitlab Status?',
       default: true
-    }];
+      },
+      {
+        type: 'input',
+        name: 'gitLabName',
+        validate: function (input) {
+          if (/^([a-zA-Z0-9_]*)$/.test(input) && input != '') return true;
+          return 'Your username is mandatory, cannot contain special characters or a blank space';
+        },
+        store: true,
+        message: 'What is your GitLab username?'
+      }
+    ];
 
     this.prompt(prompts, function (props) {
       this.props = props;
       // To access props later use this.props.someOption;
       this.confirm = props.confirm;
+      this.gitLabName = props.gitLabName;
       done();
     }.bind(this));
   },
   writing: function () {
     var done = this.async();
+    this.baseName = jhipsterVar.baseName;
+    this.webappDir = jhipsterVar.webappDir;
+
+    var html = this.fs.read(this.webappDir + '/layouts/navbar/navbar.html');
+
+    var $ = cheerio.load(html);
+
+    var navbar = $('ul.nav.navbar-nav.navbar-right');
+
+    navbar.append(  "<li>\n" +
+                        " <a href=\"https://gitlab.com/" +this.gitLabName + "/" + this.baseName + "/commits/master\">\n" +
+                            " <img alt=\"build status\" src=\"https://gitlab.com/" + this.gitLabName +"/" + this.baseName + "/badges/master/build.svg\"/> \n" +
+                        "</a>\n" +
+                    " </li> \n" +
+                  "            ");
+
+    this.fs.write(this.webappDir + '/layouts/navbar/navbar.html', $.html());
   }
+
 });
 
 
