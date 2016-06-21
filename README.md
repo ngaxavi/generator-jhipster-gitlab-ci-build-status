aea2ee02196a8f5561b0bdfb908bb012509248d8
Xavier Ngansop <xng@cs.uni-kassel.de>
2016-06-21 17:24:50 +0200

    add some elements of the structure

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # generator-jhipster-status-ci
+[![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url]
+> JHipster module, 
+
+# Introduction
+
+This is a [JHipster](http://jhipster.github.io/) module, that is meant to be used in a JHipster application.
+
+# Prerequisites
+
+As this is a [JHipster](http://jhipster.github.io/) module, we expect you have JHipster and its related tools already installed:
+
+- [Installing JHipster](https://jhipster.github.io/installation.html)
+
+# Installation
+
+To install this module:
+
+```bash
+npm install -g generator-jhipster-status-ci
+```
+
+To update this module:
+```bash
+npm update -g generator-jhipster-status-ci
+```
+
+# Usage
+
+# License
+
+Apache-2.0 © [nxkn ljsoc]
+
+[npm-image]: https://img.shields.io/npm/v/generator-jhipster-test.svg
+[npm-url]: https://npmjs.org/package/generator-jhipster-test
+[travis-image]: https://travis-ci.org/xng/generator-jhipster-test.svg?branch=master
+[travis-url]: https://travis-ci.org/xng/generator-jhipster-test
+[daviddm-image]: https://david-dm.org/xng/generator-jhipster-test.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/xng/generator-jhipster-module
--- generators/app/index.js
+++ generators/app/index.js
@@ -1,23 +1,52 @@
-"use strict";
+'use strict';
 
-var yeoman = require("yeoman-generator");
-var chalk = require("chalk");
-var yosay = require("yosay");
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+var packageJson = require(__dirname + '/../../package.json');
+
+// stores JHipster variables
+var jhipsterVar = {moduleName: 'ciStatus'};
+
+// stores JHipster functions
+var jhipsterFunc = {};
 
 module.exports = yeoman.Base.extend({
-    templates: function(args) {
-        this.composeWith('jhipster:modules', {
-            options: {
-                jhipsterVar: jhipsterVar,
-                jhipsterFunc: jhipsterFunc
-            }
-        });
+  initializing: {
+    templates: function (args) {
+      this.composeWith('jhipster:modules', {
+        options: {
+          jhipsterVar: jhipsterVar,
+          jhipsterFunc: jhipsterFunc
+        }
+      });
     },
-    prompting: function () {
-        var done = this.async();
-
-        this.log(yosay("Welcome to the " + chalk.red('JHipster CI Gitlab Status') + "generator!"))
+    displayLogo: function () {
+      this.log(yosay('Welcome to the ' + chalk.red('JHipster Continuous Integration Status') + ' Generator! ' + chalk.yellow('v' + packageJson.version + '\n')))
     }
+  },
+  prompting: function () {
+    var done = this.async();
+
+    //TODO: Check if JHipster application contains the gitlab-ci file
+
+    var prompts = [{
+      type: 'confirm',
+      name: 'confirm',
+      message: 'Would you like to enable Gitlab Status?',
+      default: true
+    }];
+
+    this.prompt(prompts, function (props) {
+      this.props = props;
+      // To access props later use this.props.someOption;
+      this.confirm = props.confirm;
+      done();
+    }.bind(this));
+  },
+  writing: function () {
+    var done = this.async();
+  }
 });
 
 
